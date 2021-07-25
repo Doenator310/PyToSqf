d5301c79564097c5a257dc34c8198947a48ed437
Doenator <david@davidhenkel.de>
2021-07-25 22:49:34 +0200

    Added more test code

--- sample.sqf.py
+++ sample.sqf.py
@@ -6,7 +6,6 @@ def getAllLootMarkers():
     ENGINE.systemchat(str(len(markerList)))
     return markerList
 
-
 lootMarkerList = getAllLootMarkers()
 
 ENGINE.systemchat(str(getAllLootMarkers()))
@@ -15,7 +14,14 @@ for marker in lootMarkerList:
     marker.setMarkerColor("ColorYellow");
     marker.setMarkerAlpha(1)
     marker.setMarkerSizeLocal (10, 10)
+await ( isNil("WEATHER_MODULE_LOADED") == False );
+STATIC.publicVariable("WEATHER_MODULE_LOADED")
+firstInit = False
+if(isNil("SOME_MODULE_LOADED") == True):
+    STATIC.publicVariable("SOME_MODULE_LOADED")
+    firstInit = True
 
+fun = 1
 await (fun > 1 and fun == 0);
 
 
--- sqfResult.tmp.sqf
+++ sqfResult.tmp.sqf
@@ -19,4 +19,12 @@ systemchat("TEEEEST!");
    _marker setMarkerAlpha(1);
    _marker setMarkerSizeLocal[10,10];
 } forEach _lootMarkerList;
+waitUntil {isNil("WEATHER_MODULE_LOADED") == false};
+publicVariable("WEATHER_MODULE_LOADED");
+private _firstInit = false;
+if (isNil("SOME_MODULE_LOADED") == true) then {
+   publicVariable("SOME_MODULE_LOADED");
+   _firstInit = true;
+};
+private _fun = 1;
 waitUntil {(_fun > 1)&&(_fun == 0)};
